65657264fe51c290296b31465a57d38c625c8132
Sergey Bronnikov <sergeyb@openvz.org>
2015-07-16 00:15:48 +0300

    Update links to ISO images, update packages to remove

--- openvz-centos-6.6/scripts/compact.sh
+++ openvz-centos-6.6/scripts/compact.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yum -y remove glibc-headers glibc-devel libgomp mpfr cloog-ppl ppl kernel-devel kernel-headers wget cpp gcc perl*
+yum -y remove glibc-headers glibc-devel libgomp mpfr cloog-ppl ppl kernel-devel kernel-headers cpp gcc
 yum clean all
 
 dd if=/dev/zero of=/junk bs=1M
