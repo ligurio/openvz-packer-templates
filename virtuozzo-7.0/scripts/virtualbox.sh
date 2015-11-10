1971178349d86d69cc9fe87b30bf8421c1204c26
Sergey Bronnikov <sergeyb@openvz.org>
2015-11-10 15:45:15 +0300

    Update post-install scripts: 	- zeroing /, /boot and /vz partitions 	- cleanup log files in /var/log 	- include zerodisk.sh to template.json

--- virtuozzo-7.0/scripts/virtualbox.sh
+++ virtuozzo-7.0/scripts/virtualbox.sh
@@ -1,11 +1,7 @@
 VBOX_VERSION=$(cat /home/openvz/.vbox_version)
 
-# required for VirtualBox 4.3.26
-yum install -y bzip2
-
 cd /tmp
 mount -o loop /home/openvz/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/openvz/VBoxGuestAdditions_*.iso
-
--- virtuozzo-7.0/scripts/zerodisk.sh
+++ virtuozzo-7.0/scripts/zerodisk.sh
@@ -1,3 +1,9 @@
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-
+echo 'Zeroing partitions'
+p="/ /boot /vz"
+for i in $p;
+do
+	count=`df --sync -kP $i | tail -n1 | awk -F ' ' '{print $4}'`;
+	let count--
+	dd if=/dev/zero of=/boot/file bs=1024 count=$count;
+	rm -f $i/file;
+done;
