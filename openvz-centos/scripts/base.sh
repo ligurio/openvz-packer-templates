f12a88f65e2f58c9c85bab5e04bd6dc0fca2aa0a
Sergey Bronnikov <sergeyb@openvz.org>
2015-11-11 19:21:26 +0300

    Switch to CentOS 6.7

--- openvz-centos/scripts/base.sh
+++ openvz-centos/scripts/base.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eux
 
-rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 curl http://ftp.openvz.org/openvz.repo > /etc/yum.repos.d/OpenVZ.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 yum install -y vzkernel vzctl vzquota ploop
