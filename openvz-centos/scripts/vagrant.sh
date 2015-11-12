aa1b687cd9c83b7d4f082b64fa9f10b0a10e0ed7
Sergey Bronnikov <sergeyb@openvz.org>
2015-11-12 11:02:35 +0300

    Change username and password from 'vagrant' to 'openvz'

--- openvz-centos/scripts/vagrant.sh
+++ openvz-centos/scripts/vagrant.sh
@@ -1,6 +1,6 @@
 date > /etc/vagrant_box_build_time
 
-mkdir -pm 700 /home/vagrant/.ssh
-curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+mkdir -pm 700 /home/openvz/.ssh
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/openvz/.ssh/authorized_keys
+chmod 0600 /home/openvz/.ssh/authorized_keys
+chown -R openvz:openvz /home/openvz/.ssh
