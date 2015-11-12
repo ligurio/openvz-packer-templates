7057f4ffd30a00ba61ebb42992b5e65c1f2bb0cd
Sergey Bronnikov <sergeyb@openvz.org>
2015-11-12 14:16:40 +0300

    Zeroing of space inside virtual machine doesn't reduce box size

--- openvz-centos/scripts/compact.sh
+++ openvz-centos/scripts/compact.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-yum -y remove glibc-headers glibc-devel libgomp mpfr cloog-ppl ppl kernel-devel kernel-headers cpp gcc
 yum clean all
-
-dd if=/dev/zero of=/junk bs=1M
-rm -f /junk
 
 sync
