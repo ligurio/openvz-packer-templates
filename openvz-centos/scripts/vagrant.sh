468a768c5235687e7a8833173c78b0c88bae88fb
Mikhail Zholobov <legal90@gmail.com>
2015-11-21 19:11:05 +0200

    Replace $HOME with $HOME_DIR custom env variable
    Shell provisioner is running from user root, so $HOME will be resolved to "/root"
    We have to set custom env variable, for example $HOME_DIR, and use it instead.

--- openvz-centos/scripts/vagrant.sh
+++ openvz-centos/scripts/vagrant.sh
@@ -1,6 +1,6 @@
 date > /etc/vagrant_box_build_time
 
-mkdir -pm 700 $HOME/.ssh
-curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $HOME/.ssh/authorized_keys
-chmod 0600 $HOME/.ssh/authorized_keys
-chown -R openvz:openvz $HOME/.ssh
+mkdir -pm 700 $HOME_DIR/.ssh
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $HOME_DIR/.ssh/authorized_keys
+chmod 0600 $HOME_DIR/.ssh/authorized_keys
+chown -R openvz:openvz $HOME_DIR/.ssh
