841795335f9adc41812dc57837942e0436b14c46
Mikhail Zholobov <legal90@gmail.com>
2015-11-22 10:59:47 +0200

    Fix cleanup.sh: remove udev rules for network devices
    - Network udev rules and MAC addresses from network interface configs have to be deleted. Each Vagrant VM
    has an unique MAC-address, so network interface should not be bounded to some non-existing MAC.
    
    - Usually log files are not cleaned up, because they could be useful for end used of the box.
    
    - Virtuozzo: these packages are not installed: "gtk2", "hicolor-icon-theme", "avahi", "bitstream-vera-fonts".
    Deletion could be skipped.
    
    - Virtuozzo: there such files or dirs: "/tmp/rubygems-*". Deletion could be skipped.
    
    The script has been ported from Bento project: https://github.com/chef/bento/

--- openvz-centos/scripts/cleanup.sh
+++ openvz-centos/scripts/cleanup.sh
@@ -1,3 +1,15 @@
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+#!/bin/sh -eux
+
+# Remove development and kernel source packages
+yum -y remove gcc cpp kernel-devel kernel-headers perl
 yum -y clean all
-rm -rf /tmp/rubygems-*
+
+# Clean up network interface persistence
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+
+for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
+    if [ "`basename $ndev`" != "ifcfg-lo" ]; then
+        sed -i '/^HWADDR/d' "$ndev"
+        sed -i '/^UUID/d' "$ndev"
+    fi
+done
