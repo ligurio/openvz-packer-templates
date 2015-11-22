f9df69c5ad486e6d732d21e3c615691aeec6730e
Mikhail Zholobov <legal90@gmail.com>
2015-11-22 10:11:00 +0200

    Install kernel packages via kickstart, before the reboot
    Otherwise, kernel modules from the guest tools will be installed for the base kernel, so
    they won't be available in Vagrant VMs.

--- openvz-centos/scripts/base.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-
-curl http://ftp.openvz.org/openvz.repo > /etc/yum.repos.d/OpenVZ.repo
-rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
-yum install -y vzkernel vzctl vzquota ploop
