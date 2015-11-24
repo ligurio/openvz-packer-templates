5c1341e3794b98c3198aa71d5f27160975be295d
Sergey Bronnikov <sergeyb@openvz.org>
2015-11-24 10:54:58 +0300

    Replace openvz user by vagrant

--- virtuozzo-7.0/scripts/vagrant.sh
+++ virtuozzo-7.0/scripts/vagrant.sh
@@ -3,4 +3,4 @@ date > /etc/vagrant_box_build_time
 mkdir -pm 700 $HOME_DIR/.ssh
 curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $HOME_DIR/.ssh/authorized_keys
 chmod 0600 $HOME_DIR/.ssh/authorized_keys
-chown -R openvz:openvz $HOME_DIR/.ssh
+chown -R vagrant:vagrant $HOME_DIR/.ssh
